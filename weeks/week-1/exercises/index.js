9cf5f0669eef4fbe5d615d180257d377fa89b3c9
Vanessa Arruda <adm.vanessasouza@gmail.com>
2023-09-28 13:00:26 +0200

    added team exercise resolution on team session 1

--- weeks/week-1/exercises/index.js
+++ weeks/week-1/exercises/index.js
@@ -5,23 +5,30 @@ Print out a greeting such as "Welcome to Technigo Cafe! What would you like to o
 Where Technigo Cafe is replaced by the name of your cafe that is stored in the variable.
 */
 
-const cafeName = "Technigo Cafe";
-
-// TEMPLATE LITERALS === backtick approach’
-
+const cafeName = "Cherry Cafe";
 console.log(`Welcome to ${cafeName}! What would you like to order today?`);
 
 /*
 2)
 Create a variable that stores the price of a coffee.
-Create a variable that stores how many coffees the customer wants.
-Print out the total price such as "There you go, that'll be 10 euros"
+*/
+
+let coffeePrice = 2;
+/*
+Create a variable that stores how many coffees the customer wants.*/
+
+const coffeeAmount = prompt("How many cups of coffee do you want? type a number");
+
+/* Print out the total price such as "There you go, that'll be 10 euros"
 Where 10 is replaced by the calculation of the total price.
 */
 
+const coffeeTotal = coffeePrice * coffeeAmount;
+console.log(`There you go, that'll be ${coffeeTotal} euros. Thank you!`);
+
 // Good practice Approach
 
-let coffeePrice = 335; 
+coffeePrice = 335; 
 // Aparently super expensive coffee!
 
 const numberOfCoffees = 10;
@@ -42,11 +49,8 @@ Print out "You said this coffee is the best, that was actually true"
 Where true is replaced by your varible
 */
 
-let isBestCofee = true;
-console.log(
-  `You said this coffee is the best, that was actually ${isBestCofee}`
-);
-
+const costumerLike = true;
+console.log(`You said this coffee is the best, that was actually ${costumerLike}`);
 /*
 4)
 Create a variable called cafeGuests, that shows us how many cafeGuests we have.
@@ -55,14 +59,18 @@ Print it out.
 assign it a new value.
 Print it out. => This should give you the new value.
 */
+let cafeGuests = 39;
+console.log(`Amount of Guests in the Cafe now is: ${cafeGuests}`);
 
-let cafeGuests = 10;
+cafeGuests = 10;
 console.log(cafeGuests);
 cafeGuests = false;
 console.log(cafeGuests);
 cafeGuests = ["I", "love", "Hip Hop"];
 console.log(cafeGuests);
 
+cafeGuests = 45;
+console.log(`Amount of Guests in the Cafe now is: ${cafeGuests}`);
 /*
 
 5)
@@ -72,8 +80,13 @@ Print it out.
 assign it a new value.
 Print it out. => This should give you an error because it shouldn't be able to be changed.
 */
+let maxGuests = 50;
+console.log(`Amount of Guests in the Cafe now is: ${maxGuests}`);
 
-const maxGuests = 10;
+/*maxGuests = 30;
+console.log(`Amount of Guests in the Cafe now is: ${maxGuests}`)*/
+
+maxGuests = 10;
 console.log(maxGuests);
 //maxGuests = "I also love rock n roll!";
 
@@ -82,18 +95,16 @@ console.log(maxGuests);
 Create a variable that stores a string.
 Print out that string in only UPPERCASE letters.
 */
-let favoriteBand = "black sabbath";
-console.log(favoriteBand);
-let upperCase = favoriteBand.toUpperCase();
-console.log(upperCase);
+let str = "TeAm CheRRy";
+const upperStr = str.toUpperCase();
+console.log(upperStr);
 
 /*
 7)
 Print out the same string in only lowercase letters.
 */
-
-console.log(upperCase.toLowerCase());
-
+const lowerStr = str.toLowerCase();
+console.log(lowerStr);
 /*
 8) **BONUS**
 Print out the string "Today we have a special summer deal!". 
@@ -105,3 +116,7 @@ Should give you => "Today we have a special winter deal!"
 let specialDeal = "Today we have a special summer deal!";
 let updatedDeal = specialDeal.replace("summer", "winter");
 console.log(updatedDeal);
+let season = "summer";
+newText = season.replace("summer", "winter");
+
+console.log(`Today we have a special ${newText} deal!`);
