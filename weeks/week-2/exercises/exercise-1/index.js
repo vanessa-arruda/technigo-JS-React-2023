20ef6da95e9d2137ed70bb42ddc7fdcf397872aa
Vanessa Arruda <adm.vanessasouza@gmail.com>
2023-10-05 14:51:18 +0200

    solved exercises

--- weeks/week-2/exercises/exercise-1/index.js
+++ weeks/week-2/exercises/exercise-1/index.js
@@ -1,39 +1,59 @@
-// Select the element with the class "box" and store it in the "box" variable.
-const box = document.querySelector(".box");
+// // Select the element with the class "box" and store it in the "box" variable.
+// const box = document.querySelector(".box");
 
-// Select all input elements with the name attribute set to "theme" and store them in the "themeRadios" variable.
-const themeRadios = document.querySelectorAll('input[name="theme"]');
+// // Select all input elements with the name attribute set to "theme" and store them in the "themeRadios" variable.
+// const themeRadios = document.querySelectorAll('input[name="theme"]');
 
-// Loop through each radio button in the "themeRadios" NodeList.
-themeRadios.forEach((radio) => {
-  // Add an event listener to each radio button to listen for the "change" event.
-  // This event triggers when the radio button's state changes (i.e., when it's selected).
-  radio.addEventListener("change", function () {
-    let color; // Declare a variable to store the background color value.
+// // Loop through each radio button in the "themeRadios" NodeList.
+// themeRadios.forEach((radio) => {
+//   // Add an event listener to each radio button to listen for the "change" event.
+//   // This event triggers when the radio button's state changes (i.e., when it's selected).
+//   radio.addEventListener("change", function () {
+//     let color; // Declare a variable to store the background color value.
 
-    // Check the value of the currently selected radio button (using 'this') and set the "color" variable accordingly.
-    if (this.value === "dark") {
-      color = "darkgray";
-    } else if (this.value === "light") {
-      color = "lightblue";
-    } else if (this.value === "crazy") {
-      color = "purple";
-    } else {
-      color = "lightgreen"; // Default color.
-    }
+//     // Check the value of the currently selected radio button (using 'this') and set the "color" variable accordingly.
+//     if (this.value === "dark") {
+//       color = "darkgray";
+//     } else if (this.value === "light") {
+//       color = "lightblue";
+//     } else if (this.value === "crazy") {
+//       color = "purple";
+//     } else {
+//       color = "lightgreen"; // Default color.
+//     }
 
-    // Add the "color-transition" class to the "box" element.
-    // This class presumably contains CSS that provides a smooth color transition effect.
-    box.classList.add("color-transition");
+//     // Add the "color-transition" class to the "box" element.
+//     // This class presumably contains CSS that provides a smooth color transition effect.
+//     box.classList.add("color-transition");
 
-    // Change the background color of the "box" element to the value stored in the "color" variable.
-    box.style.backgroundColor = color;
+//     // Change the background color of the "box" element to the value stored in the "color" variable.
+//     box.style.backgroundColor = color;
 
-    // Set a timeout to execute a function after a delay of 300 milliseconds.
-    setTimeout(() => {
-      // After the delay, remove the "color-transition" class from the "box" element.
-      // This might be done to ensure that subsequent changes to the box's style don't have the transition effect.
-      box.classList.remove("color-transition");
-    }, 300); // 300 milliseconds delay.
-  });
+//     // Set a timeout to execute a function after a delay of 300 milliseconds.
+//     setTimeout(() => {
+//       // After the delay, remove the "color-transition" class from the "box" element.
+//       // This might be done to ensure that subsequent changes to the box's style don't have the transition effect.
+//       box.classList.remove("color-transition");
+//     }, 300); // 300 milliseconds delay.
+//   });
+// });
+// console.log("Start Here 😉");
+
+const boxContainer = document.getElementById("box-container")
+function changeStyle(backgroundColor, color) {
+    boxContainer.style.backgroundColor = backgroundColor;
+    document.getElementById("first-text").style.color = color;
+}
+
+document.getElementById("dark").addEventListener("change", () => {
+    changeStyle('#2d2d2d', '#e3e3e3');
+
+});
+
+document.getElementById("light").addEventListener("change", () => {
+    changeStyle('#e3e3e3', '#000000');
+});
+
+document.getElementById("crazy").addEventListener("change", () => {
+    changeStyle('#bf1da4', '#fbff00');
 });
--- weeks/week-2/exercises/exercise-2/index.js
+++ weeks/week-2/exercises/exercise-2/index.js
@@ -12,173 +12,261 @@ const theCoders = (driver, navigator) => {
     `The awesome coders for today is the driver ${driver} and the navigator ${navigator}`
   );
 };
-theCoders("nella", "tom");
-console.log("----");
+// theCoders("nella", "tom");
+// console.log("----");
+theCoders("Jennie", "Alfons");
+/*
+2)
+Pass in only ONE name when you call the function and see what happens.
+(HINT, you should get undefined...)
+*/
+
+theCoders("Jennie");
 
 /*
-  2)
-  Pass in only ONE name when you call the function and see what happens.
-  (HINT, you should get undefined...)
-  */
-console.log("Iteration 02");
-console.log("This Iteration is to showcase an undefined value on the console");
-console.log("----");
+//   2)
+//   Pass in only ONE name when you call the function and see what happens.
+//   (HINT, you should get undefined...)
+//   */
+// console.log("Iteration 02");
+// console.log("This Iteration is to showcase an undefined value on the console");
+// console.log("----");
+// /*
+//   3) 
+//   create a function that checks which name is the longest when comparing the coders 
+//   and print out: "The driver has the longest name." or "The navigator has the longest name" 
+//   or if the names ar the same length "The driver and the navigator has equally long names"
+//   */
+// console.log("Iteration 03");
+// const coderComparison = (driver, navigator) => {
+//   let driverLength = driver.length;
+//   let navLength = navigator.length;
+//   if (driverLength > navLength) {
+//     console.log("The driver has the longest name.");
+//   } else if (driverLength < navLength) {
+//     console.log("The navigator has the longest name.");
+//   } else {
+//     console.log("The driver and the navigator have equally long names.");
+//   }
+// };
+// coderComparison("nella", "tom");
+// coderComparison("nella", "michelangelo");
+// coderComparison("mathilda", "nella");
+// console.log("----");
+
+// /*
+//   4) 
+//   to get user input you can use the prompt() and then store that into a variable.
+//   Such as 
+//   let userAge = prompt('How old are you?')
+//   Create a function that prompts the user for its age, then depending on the age will alert back different messages such as 
+//   alert('wow you are 33! that's young!')
+//   or alert('23! Younger than Zlatan') or something like that.
+//   Add as many conditonals as you want.
+//   */
+
+// console.log("Iteration 04");
+
+// console.log("---- Version 01");
+// console.log("Using if-else-if-else statements");
+// let tellMeYourAge = () => {
+//   let userAge = prompt("How old are you? - Regular Conditional");
+//   console.log(userAge);
+//   if (userAge === 21) {
+//     console.log(`You are 21 years old! - to be young`);
+//   } else if (userAge >= 21 && userAge <= 30) {
+//     console.log(`Enjoy your 20's - :)`);
+//   } else if (userAge >= 31 && userAge <= 80) {
+//     console.log(`Always try to stay active both physically and mentally!`);
+//   } else if (userAge < 20) {
+//     console.log(`Enjoy your early School years`);
+//   } else {
+//     console.log(`Sit back and relax!`);
+//   }
+// };
+// //tellMeYourAge();
+
+// console.log("---- Version 02");
+// console.log("Using switch");
+// let tellMeYourAgeSwitch = () => {
+//   let userAge = prompt("How old are you? - Using Switch Statement");
+//   console.log(userAge);
+
+//   switch (parseInt(userAge)) {
+//     case 21:
+//       console.log(`You are 21 years old! - to be young`);
+//       break;
+//     case userAge >= 21 && userAge <= 30:
+//       console.log(`Enjoy your 20's - :)`);
+//       break;
+//     case userAge >= 31 && userAge <= 80:
+//       console.log(`Always try to stay active both physically and mentally!`);
+//       break;
+//     case userAge < 20:
+//       console.log(`Enjoy your early School years`);
+//       break;
+//     default:
+//       console.log(`Sit back and relax!`);
+//       break;
+//   }
+// };
+
+// //tellMeYourAgeSwitch();
+// console.log("----");
+
+// console.log("---- Version 03");
+// console.log("Using a ternary operator");
+// let tellMeYourAgeTernary = () => {
+//   let userAge = prompt("How old are you? - Using Ternary");
+//   console.log(userAge);
+
+//   userAge === "21"
+//     ? console.log("You are 21 years old! - to be young")
+//     : userAge >= 21 && userAge <= 30
+//     ? console.log("Enjoy your 20's - :)")
+//     : userAge >= 31 && userAge <= 80
+//     ? console.log("Always try to stay active both physically and mentally!")
+//     : userAge < 20
+//     ? console.log("Enjoy your early School years")
+//     : console.log("Sit back and relax!");
+// };
+
+// //tellMeYourAgeTernary();
+
+// console.log("---- Version 03");
+// console.log("Using if-else-if-else statements with typeChecking");
+// let tellMeYourAgeWithValidation = () => {
+//   let userAge = prompt("How old are you? - With Validation");
+
+//   // Check if userAge is a number
+//   if (isNaN(userAge)) {
+//     console.log("Invalid input. Please enter a number.");
+//     alert("Invalid input. Please enter a number.");
+//     tellMeYourAgeWithValidation();
+//     return;
+//   }
+
+//   // The userAge is a valid number, proceed with the logic
+//   userAge = parseInt(userAge);
+
+//   if (userAge === 21) {
+//     console.log("You are 21 years old! - to be young");
+//   } else if (userAge >= 21 && userAge <= 30) {
+//     console.log("Enjoy your 20's - :)");
+//   } else if (userAge >= 31 && userAge <= 80) {
+//     console.log("Always try to stay active both physically and mentally!");
+//   } else if (userAge < 20) {
+//     console.log("Enjoy your early School years");
+//   } else {
+//     console.log("Sit back and relax!");
+//   }
+// };
+
+// //tellMeYourAgeWithValidation();
+// console.log("----");
+
+// console.log("Iteration 05");
+// const calculationTest = () => {
+//   let calculationString = prompt("what is 5 + 6?");
+//   let calculationNumber = parseInt(calculationString);
+//   if (calculationNumber === 11) {
+//     console.log("Yay Correct");
+//   } else {
+//     console.log("Noooo, the answer is 11");
+//   }
+//   /*
+//     5)
+//     In this function we want to give the user a mathematical calculation to 
+//     give us the answer to. Something like prompt('What is 5 + 6?')  
+//     If the answer is correct, congratulate the user. 'Yay! Correct'
+//     If not, give the user the correct answer so they can learn. 'Noooo, the answer is 11'
+//     You choose if to use addition, subtraction, multiplication or division. 
+//     */
+// };
+// calculationTest(10, 20);
+// console.log("----");
+// /*
+//   6) **BONUS**
+//   Make the calculation machine we just made show random calculations everytime you invoke the function.
+//   But hey, maybe limit the randomness to be numbers between 0-10?
+//   */
 /*
-  3) 
-  create a function that checks which name is the longest when comparing the coders 
-  and print out: "The driver has the longest name." or "The navigator has the longest name" 
-  or if the names ar the same length "The driver and the navigator has equally long names"
-  */
-console.log("Iteration 03");
-const coderComparison = (driver, navigator) => {
-  let driverLength = driver.length;
-  let navLength = navigator.length;
-  if (driverLength > navLength) {
-    console.log("The driver has the longest name.");
-  } else if (driverLength < navLength) {
-    console.log("The navigator has the longest name.");
-  } else {
-    console.log("The driver and the navigator have equally long names.");
-  }
-};
-coderComparison("nella", "tom");
-coderComparison("nella", "michelangelo");
-coderComparison("mathilda", "nella");
-console.log("----");
+3) 
+create a function that checks which name is the longest when comparing the coders 
+and print out: "The driver has the longest name." or "The navigator has the longest name" 
+or if the names ar the same length "The driver and the navigator has equally long names"
+*/
+const longestName = (driver, navigator) => {
 
+  if(driver.length > navigator.length){
+    console.log(`The driver ${driver} has the longest name.`);
+  } else if(navigator.length > driver.length) {
+    console.log(`The navigator ${navigator} has the longest name`)
+  } else{
+    console.log(`The driver ${driver} and the navigator ${navigator} has equally long names`);
+  }
+}
+
+longestName("Vanessa", "Julia");
 /*
-  4) 
-  to get user input you can use the prompt() and then store that into a variable.
-  Such as 
-  let userAge = prompt('How old are you?')
-  Create a function that prompts the user for its age, then depending on the age will alert back different messages such as 
-  alert('wow you are 33! that's young!')
-  or alert('23! Younger than Zlatan') or something like that.
-  Add as many conditonals as you want.
-  */
+4) 
+to get user input you can use the prompt() and then store that into a variable.
+Such as 
+let userAge = prompt('How old are you?')
+Create a function that prompts the user for its age, then depending on the age will alert back different messages such as 
+alert('wow you are 33! that's young!')
+or alert('23! Younger than Zlatan') or something like that.
+Add as many conditonals as you want.
+*/
+const askUserAge = () => {
 
-console.log("Iteration 04");
-
-console.log("---- Version 01");
-console.log("Using if-else-if-else statements");
-let tellMeYourAge = () => {
-  let userAge = prompt("How old are you? - Regular Conditional");
-  console.log(userAge);
-  if (userAge === 21) {
-    console.log(`You are 21 years old! - to be young`);
-  } else if (userAge >= 21 && userAge <= 30) {
-    console.log(`Enjoy your 20's - :)`);
-  } else if (userAge >= 31 && userAge <= 80) {
-    console.log(`Always try to stay active both physically and mentally!`);
-  } else if (userAge < 20) {
-    console.log(`Enjoy your early School years`);
-  } else {
-    console.log(`Sit back and relax!`);
-  }
-};
-//tellMeYourAge();
-
-console.log("---- Version 02");
-console.log("Using switch");
-let tellMeYourAgeSwitch = () => {
-  let userAge = prompt("How old are you? - Using Switch Statement");
-  console.log(userAge);
-
-  switch (parseInt(userAge)) {
-    case 21:
-      console.log(`You are 21 years old! - to be young`);
-      break;
-    case userAge >= 21 && userAge <= 30:
-      console.log(`Enjoy your 20's - :)`);
-      break;
-    case userAge >= 31 && userAge <= 80:
-      console.log(`Always try to stay active both physically and mentally!`);
-      break;
-    case userAge < 20:
-      console.log(`Enjoy your early School years`);
-      break;
-    default:
-      console.log(`Sit back and relax!`);
-      break;
-  }
-};
-
-//tellMeYourAgeSwitch();
-console.log("----");
-
-console.log("---- Version 03");
-console.log("Using a ternary operator");
-let tellMeYourAgeTernary = () => {
-  let userAge = prompt("How old are you? - Using Ternary");
-  console.log(userAge);
-
-  userAge === "21"
-    ? console.log("You are 21 years old! - to be young")
-    : userAge >= 21 && userAge <= 30
-    ? console.log("Enjoy your 20's - :)")
-    : userAge >= 31 && userAge <= 80
-    ? console.log("Always try to stay active both physically and mentally!")
-    : userAge < 20
-    ? console.log("Enjoy your early School years")
-    : console.log("Sit back and relax!");
-};
-
-//tellMeYourAgeTernary();
-
-console.log("---- Version 03");
-console.log("Using if-else-if-else statements with typeChecking");
-let tellMeYourAgeWithValidation = () => {
-  let userAge = prompt("How old are you? - With Validation");
-
-  // Check if userAge is a number
-  if (isNaN(userAge)) {
-    console.log("Invalid input. Please enter a number.");
-    alert("Invalid input. Please enter a number.");
-    tellMeYourAgeWithValidation();
-    return;
-  }
-
-  // The userAge is a valid number, proceed with the logic
+  let userAge = prompt(`How old are you?`);
   userAge = parseInt(userAge);
 
-  if (userAge === 21) {
-    console.log("You are 21 years old! - to be young");
-  } else if (userAge >= 21 && userAge <= 30) {
-    console.log("Enjoy your 20's - :)");
-  } else if (userAge >= 31 && userAge <= 80) {
-    console.log("Always try to stay active both physically and mentally!");
-  } else if (userAge < 20) {
-    console.log("Enjoy your early School years");
-  } else {
-    console.log("Sit back and relax!");
+  if(userAge < 15){
+    alert(`${userAge}! You are just starting life!`);
+  } else if(userAge >= 15 && userAge <= 25){
+    alert(`Wow! ${userAge} is a beautiful phase! Enjoy at its max!`);
+  } else if(userAge > 25 && userAge <= 35){
+    alert(`${userAge}...That's when you still have the humor of a teenager but starts to have the wisdom of an experienced person.`)
+  } else{
+    alert("After certain age, you will become more and more tired, it's ok.");
   }
-};
+}
+askUserAge();
 
-//tellMeYourAgeWithValidation();
-console.log("----");
-
-console.log("Iteration 05");
 const calculationTest = () => {
-  let calculationString = prompt("what is 5 + 6?");
-  let calculationNumber = parseInt(calculationString);
-  if (calculationNumber === 11) {
-    console.log("Yay Correct");
-  } else {
-    console.log("Noooo, the answer is 11");
-  }
   /*
-    5)
-    In this function we want to give the user a mathematical calculation to 
-    give us the answer to. Something like prompt('What is 5 + 6?')  
-    If the answer is correct, congratulate the user. 'Yay! Correct'
-    If not, give the user the correct answer so they can learn. 'Noooo, the answer is 11'
-    You choose if to use addition, subtraction, multiplication or division. 
-    */
-};
-calculationTest(10, 20);
-console.log("----");
-/*
-  6) **BONUS**
-  Make the calculation machine we just made show random calculations everytime you invoke the function.
-  But hey, maybe limit the randomness to be numbers between 0-10?
+  5)
+  In this function we want to give the user a mathematical calculation to 
+  give us the answer to. Something like prompt('What is 5 + 6?')  
+  If the answer is correct, congratulate the user. 'Yay! Correct'
+  If not, give the user the correct answer so they can learn. 'Noooo, the answer is 11'
+  You choose if to use addition, subtraction, multiplication or division. 
   */
+
+  let min = Math.floor(Math.random() * 11);
+  let max = Math.floor(Math.random() * 11);
+
+  let userAnswer = prompt(
+    `What is ${min} * ${max}?
+    type your answer (number) below:`
+    );
+  userAnswer = parseInt(userAnswer);
+  const calcAnswer = min * max;
+  if(userAnswer === calcAnswer){
+    alert(`${userAnswer}! You're right! That's correct! 🥳`);
+  } else{
+    alert(`💥 No! The answer is ${calcAnswer}`);
+  }
+};
+
+calculationTest();
+
+/*
+6) **BONUS**
+Make the calculation machine we just made show random calculations everytime you invoke the function.
+But hey, maybe limit the randomness to be numbers between 0-10?
+*/
+
+
